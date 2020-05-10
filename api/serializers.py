b21be08efe36b537092b2ec6a40abd774d4b38a9
kharumax <kharumax@gmail.com>
2020-05-10 15:44:04 +0900

    Fin DRF backend

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 
 from .models import *
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id","username","password")
+        extra_kwargs = {"password":{"write_only":True,"required":True}} # write_onlyなので読み込むことが出来ない（登録や更新は出来る
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ("id","title","description")
+        fields = ("id","title","description","no_of_ratings","avg_rating")
         read_only = ("id",)
 
 
